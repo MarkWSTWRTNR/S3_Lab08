ab6bd4753e731d6c68df77b231dd8778ab2df423
Wongsathorn632115035 <wongsathorn_w@cmu.ac.th>
2022-09-08 23:03:00 +0700

    4.4

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -18,8 +18,8 @@ public class Lab08Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/events")
-                        .allowedOrigins("http://localhost:3000");
+                registry.addMapping("/events").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/events/*").allowedOrigins("http://localhost:3000");
             }
         };
     }
